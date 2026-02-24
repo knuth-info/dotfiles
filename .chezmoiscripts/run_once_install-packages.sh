8d45792ed5596742a826af01ca8c03a6d1fffd11
Marcus Knuth <github@knuth.info>
2026-02-24 07:14:53 +0100

    add age 1.3.1 for debian

--- .chezmoiscripts/run_once_install-packages.sh
+++ .chezmoiscripts/run_once_install-packages.sh
@@ -7,11 +7,17 @@ if [ "$(id -u)" -ne 0 ] && command -v sudo &> /dev/null; then
     SUDO="sudo"
 fi
 
+AGE_VERSION="1.3.1"
+
 # Detect OS
 if [ -f /etc/debian_version ]; then
     $SUDO apt-get update -y
     $SUDO apt-get upgrade -y
     $SUDO apt-get install -y \
-        zsh \
-        age
+        zsh
+    curl -LO "https://github.com/FiloSottile/age/releases/download/v${AGE_VERSION}/age-v${AGE_VERSION}-linux-amd64.tar.gz"
+    tar xzf "age-v${AGE_VERSION}-linux-amd64.tar.gz"
+    $SUDO mv age/age age/age-keygen /usr/local/bin/
+    rm -rf age "age-v${AGE_VERSION}-linux-amd64.tar.gz"
 fi
+
