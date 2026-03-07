3629ca6bf105423ee4c9cfa242c53a3c0330cfc0
Marcus Knuth <github@knuth.info>
2026-03-07 07:57:01 +0100

    make onchange a tmpl to get a hash / changed go template to sha256sum all files in ansible folder and subfolders

--- .chezmoiscripts/run_onchange_after_ansible-setup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# playbook.yml hash: {{ include "ansible/playbook.yml" | sha256sum }}
-set -euo pipefail
-
-ansible-playbook --connection=local --inventory 127.0.0.1, \
-    "${CHEZMOI_SOURCE_DIR}/ansible/playbook.yml"
